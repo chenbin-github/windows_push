fcd4c29805cb977a4a4c30985fefb37de21602ef
chenbin <178710014@qq.com>
2019-04-04 22:07:32 +0800

    再次提交测试！

--- springboot-demo/src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
+++ springboot-demo/src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -9,6 +9,10 @@ public class SpringbootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
+	
+	public static void doSomething(){
+		
+	}
 
 }
 
