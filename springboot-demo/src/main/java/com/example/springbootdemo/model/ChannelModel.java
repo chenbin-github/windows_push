5dfa81cc33aab7fb492e98cdfb4a2bb5a44e2c15
chenbin <178710014@qq.com>
2019-04-05 13:30:32 +0800

    我在dev分支提交

--- springboot-demo/src/main/java/com/example/springbootdemo/model/ChannelModel.java
+++ springboot-demo/src/main/java/com/example/springbootdemo/model/ChannelModel.java
@@ -6,5 +6,9 @@ package com.example.springbootdemo.model;
  * @date 2019年4月4日 下午9:59:47  
  */
 public class ChannelModel {
+	
+	public static void main(String[] args) {
+		System.out.println("我添加了新的分支dev");
+	}
 
 }
